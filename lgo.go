72ef9acf953ee7a2de1e578c30bd43caf6d075c9
reus <reusee@gmail.com>
2013-12-30 00:54:09 +0800

    refactor argument mashaling

--- lgo.go
+++ lgo.go
@@ -10,12 +10,13 @@ package lgo
 
 void register_function(lua_State*, const char*, void*);
 void setup_message_handler(lua_State*);
+int traceback(lua_State*);
 
 */
 import "C"
 
 import (
-	"log"
+	"fmt"
 	"reflect"
 	"unsafe"
 )
@@ -26,15 +27,15 @@ type Lua struct {
 }
 
 type Function struct {
-	name  string
-	fun   interface{}
-	state *C.lua_State
+	name string
+	fun  interface{}
+	lua  *Lua
 }
 
 func NewLua() *Lua {
 	state := C.luaL_newstate()
 	if state == nil {
-		log.Fatalf("lua state create error")
+		panic("lua state create error")
 	}
 	C.luaL_openlibs(state)
 	lua := &Lua{
@@ -47,14 +48,14 @@ func NewLua() *Lua {
 func (self *Lua) RegisterFunction(name string, fun interface{}) {
 	funcType := reflect.TypeOf(fun)
 	if funcType.IsVariadic() {
-		log.Fatalf("cannot register variadic function: %v", fun)
+		self.Panic("cannot register variadic function: %v", fun)
 	}
 	cName := C.CString(name)
 	defer C.free(unsafe.Pointer(cName))
 	function := &Function{
-		fun:   fun,
-		state: self.State,
-		name:  name,
+		fun:  fun,
+		lua:  self,
+		name: name,
 	}
 	C.register_function(self.State, cName, unsafe.Pointer(function))
 	self.Functions[name] = function
@@ -63,28 +64,27 @@ func (self *Lua) RegisterFunction(name string, fun interface{}) {
 //export Invoke
 func Invoke(p unsafe.Pointer) int {
 	function := (*Function)(p)
-	state := function.state
+	lua := function.lua
 	funcType := reflect.TypeOf(function.fun)
 	// check argument count
-	argc := C.lua_gettop(state)
+	argc := C.lua_gettop(lua.State)
 	if int(argc) != funcType.NumIn() {
-		log.Fatalf("arguments not match: %v", function.fun)
+		lua.Panic("arguments not match: %v", function.fun)
 	}
 	// arguments
 	var args []reflect.Value
-	var paramType reflect.Type
 	for i := C.int(1); i <= argc; i++ {
-		args = append(args, toGoValue(state, i, paramType))
+		args = append(args, toGoValue(lua, i, funcType.In(int(i-1))))
 	}
 	// call
 	funcValue := reflect.ValueOf(function.fun)
 	// returns
 	returnValues := funcValue.Call(args)
 	for _, v := range returnValues {
-		pushGoValue(state, v)
+		pushGoValue(lua, v)
 	}
 	if len(returnValues) != funcType.NumOut() {
-		log.Fatalf("return values not match: %v", function.fun)
+		lua.Panic("return values not match: %v", function.fun)
 	}
 	return len(returnValues)
 }
@@ -92,73 +92,95 @@ func Invoke(p unsafe.Pointer) int {
 var stringType = reflect.TypeOf("")
 var intType = reflect.TypeOf(int(0))
 
-func toGoValue(state *C.lua_State, i C.int, paramType reflect.Type) (ret reflect.Value) {
+func toGoValue(lua *Lua, i C.int, paramType reflect.Type) (ret reflect.Value) {
+	state := lua.State
 	luaType := C.lua_type(state, i)
-	if luaType == C.LUA_TBOOLEAN { // boolean
+	paramKind := paramType.Kind()
+	switch paramKind {
+	case reflect.Bool:
+		if luaType != C.LUA_TBOOLEAN {
+			lua.Panic("not a boolean")
+		}
 		ret = reflect.ValueOf(C.lua_toboolean(state, i) == C.int(1))
-	} else if luaType == C.LUA_TNUMBER { // int, uint or float
-		switch paramType.Kind() {
-		case reflect.Interface:
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		if luaType != C.LUA_TNUMBER {
+			lua.Panic("not a integer")
+		}
+		ret = reflect.New(paramType).Elem()
+		ret.SetInt(int64(C.lua_tointegerx(state, i, nil)))
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		if luaType != C.LUA_TNUMBER {
+			lua.Panic("not a unsigned")
+		}
+		ret = reflect.New(paramType).Elem()
+		ret.SetUint(uint64(C.lua_tointegerx(state, i, nil)))
+	case reflect.Float32, reflect.Float64:
+		if luaType != C.LUA_TNUMBER {
+			lua.Panic("not a unsigned")
+		}
+		ret = reflect.New(paramType).Elem()
+		ret.SetFloat(float64(C.lua_tonumberx(state, i, nil)))
+	case reflect.Interface:
+		switch luaType {
+		case C.LUA_TNUMBER:
 			ret = reflect.New(intType).Elem()
 			ret.SetInt(int64(C.lua_tointegerx(state, i, nil)))
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			ret = reflect.New(paramType).Elem()
-			ret.SetInt(int64(C.lua_tointegerx(state, i, nil)))
-		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			ret = reflect.New(paramType).Elem()
-			ret.SetUint(uint64(C.lua_tointegerx(state, i, nil)))
-		default:
-			ret = reflect.New(paramType).Elem()
-			ret.SetFloat(float64(C.lua_tonumberx(state, i, nil)))
-		}
-	} else if luaType == C.LUA_TSTRING { // string or bytes
-		switch paramType.Kind() {
-		case reflect.Interface:
+		case C.LUA_TSTRING:
 			ret = reflect.New(stringType).Elem()
 			ret.SetString(C.GoString(C.lua_tolstring(state, i, nil)))
-		case reflect.String:
-			ret = reflect.New(paramType).Elem()
-			ret.SetString(C.GoString(C.lua_tolstring(state, i, nil)))
-		case reflect.Slice:
+		default:
+			lua.Panic("wrong interface argument")
+		}
+	case reflect.String:
+		if luaType != C.LUA_TSTRING {
+			lua.Panic("not a string")
+		}
+		ret = reflect.New(paramType).Elem()
+		ret.SetString(C.GoString(C.lua_tolstring(state, i, nil)))
+	case reflect.Slice:
+		switch luaType {
+		case C.LUA_TSTRING:
 			ret = reflect.New(paramType).Elem()
 			cstr := C.lua_tolstring(state, i, nil)
 			ret.SetBytes(C.GoBytes(unsafe.Pointer(cstr), C.int(C.strlen(cstr))))
-		default:
-			log.Fatalf("invalid string argument")
-		}
-	} else if luaType == C.LUA_TLIGHTUSERDATA { // pointer
-		ret = reflect.ValueOf(C.lua_topointer(state, i))
-	} else if luaType == C.LUA_TTABLE { // slice or map
-		switch paramType.Kind() {
-		case reflect.Slice: // slice
+		case C.LUA_TTABLE:
 			ret = reflect.MakeSlice(paramType, 0, 0)
 			C.lua_pushnil(state)
 			elemType := paramType.Elem()
 			for C.lua_next(state, i) != 0 {
-				ret = reflect.Append(ret, toGoValue(state, -1, elemType))
-				C.lua_settop(state, -2)
-			}
-		case reflect.Map: // map
-			ret = reflect.MakeMap(paramType)
-			C.lua_pushnil(state)
-			keyType := paramType.Key()
-			elemType := paramType.Elem()
-			for C.lua_next(state, i) != 0 {
-				ret.SetMapIndex(
-					toGoValue(state, -2, keyType),
-					toGoValue(state, -1, elemType))
+				ret = reflect.Append(ret, toGoValue(lua, -1, elemType))
 				C.lua_settop(state, -2)
 			}
 		default:
-			log.Fatalf("cannot assign lua table to %v", paramType)
+			lua.Panic("wrong slice argument")
 		}
-	} else {
-		log.Fatalf("invalid argument type")
+	case reflect.Ptr:
+		if luaType != C.LUA_TLIGHTUSERDATA {
+			lua.Panic("not a pointer")
+		}
+		ret = reflect.ValueOf(C.lua_topointer(state, i))
+	case reflect.Map:
+		if luaType != C.LUA_TTABLE {
+			lua.Panic("not a map")
+		}
+		ret = reflect.MakeMap(paramType)
+		C.lua_pushnil(state)
+		keyType := paramType.Key()
+		elemType := paramType.Elem()
+		for C.lua_next(state, i) != 0 {
+			ret.SetMapIndex(
+				toGoValue(lua, -2, keyType),
+				toGoValue(lua, -1, elemType))
+			C.lua_settop(state, -2)
+		}
+	default:
+		lua.Panic("unknown argument type")
 	}
 	return
 }
 
-func pushGoValue(state *C.lua_State, value reflect.Value) {
+func pushGoValue(lua *Lua, value reflect.Value) {
+	state := lua.State
 	switch t := value.Type(); t.Kind() {
 	case reflect.Bool:
 		if value.Bool() {
@@ -179,15 +201,15 @@ func pushGoValue(state *C.lua_State, value reflect.Value) {
 		C.lua_createtable(state, C.int(length), 0)
 		for i := 0; i < length; i++ {
 			C.lua_pushnumber(state, C.lua_Number(i+1))
-			pushGoValue(state, value.Index(i))
+			pushGoValue(lua, value.Index(i))
 			C.lua_settable(state, -3)
 		}
 	case reflect.Interface:
-		pushGoValue(state, value.Elem())
+		pushGoValue(lua, value.Elem())
 	case reflect.Ptr:
 		C.lua_pushlightuserdata(state, unsafe.Pointer(value.Pointer()))
 	default:
-		log.Fatalf("wrong return value %v %v", value, t.Kind())
+		lua.Panic("wrong return value %v %v", value, t.Kind())
 	}
 }
 
@@ -196,10 +218,18 @@ func (self *Lua) RunString(code string) {
 	defer C.free(unsafe.Pointer(cCode))
 	C.setup_message_handler(self.State)
 	if ret := C.luaL_loadstring(self.State, cCode); ret != C.int(0) {
-		log.Fatalf("%s", C.GoString(C.lua_tolstring(self.State, -1, nil)))
+		self.Panic("%s", C.GoString(C.lua_tolstring(self.State, -1, nil)))
 	}
 	ret := C.lua_pcallk(self.State, 0, 0, C.lua_gettop(self.State)-C.int(1), 0, nil)
 	if ret != C.int(0) {
-		log.Fatalf("%s", C.GoString(C.lua_tolstring(self.State, -1, nil)))
+		self.Panic("%s", C.GoString(C.lua_tolstring(self.State, -1, nil)))
 	}
+}
+
+func (self *Lua) Panic(format string, args ...interface{}) {
+	C.traceback(self.State)
+	print("============ start lua traceback ============\n")
+	fmt.Printf("%s\n", C.GoString(C.lua_tolstring(self.State, -1, nil)))
+	print("============ end lua traceback ==============\n")
+	panic(fmt.Sprintf(format, args...))
 }
